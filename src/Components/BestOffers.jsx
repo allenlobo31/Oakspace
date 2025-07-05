1c7501dd6e6e42c63b33454cebee773339bc1076
allenlobo31 <allenlobo05@gmail.com>
2025-07-05 11:54:32 +0530

    k

--- src/Components/BestOffers.jsx
+++ src/Components/BestOffers.jsx
@@ -6,8 +6,8 @@ function BestOffers() {
             <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold mb-8 text-left font-poppins">
                 Best Offers
             </h1>
-            <div className='flex flex-row lg:gap-40 md:gap-20 gap-10 items-center justify-center'>
-                <div className='lg:w-[450px] lg:h-[450px] md:w-[300px] md:h-[300px] w-[170px] h-[170px] relative mb-[70px] aspect-square'>
+            <div className='flex flex-row lg:gap-30 md:gap-20 gap-10 items-center justify-center'>
+                <div className='lg:w-[450px] lg:h-[450px] md:w-[300px] md:h-[300px] w-[160px] h-[160px] relative mb-[70px] aspect-square'>
                     <img 
                         src="https://images.cb2.com/is/image/CB2/052825_CatCarousel_Dining?&wid=800&hei=800" 
                         alt="Best Seller Item" 
@@ -18,7 +18,7 @@ function BestOffers() {
                     </h3>
                 </div>
                 
-                <div className='lg:w-[450px] lg:h-[450px]  md:w-[300px] md:h-[300px] w-[170px] h-[170px] relative mb-[70px] aspect-square'>
+                <div className='lg:w-[450px] lg:h-[450px]  md:w-[300px] md:h-[300px] w-[160px] h-[160px] relative mb-[70px] aspect-square'>
                     <img 
                         src="https://images.cb2.com/is/image/CB2/052825_CatCarousel_Dining?&wid=800&hei=800" 
                         alt="Best Seller Item" 
@@ -30,8 +30,8 @@ function BestOffers() {
                 </div>
             </div>
 
-             <div className='flex flex-row lg:gap-40 md:gap-20 gap-10 items-center justify-center lg:mt-[40px]'>
-                <div className='lg:w-[450px] lg:h-[450px] md:w-[300px] md:h-[300px] w-[170px] h-[170px] relative mb-[70px] aspect-square'>
+             <div className='flex flex-row lg:gap-30 md:gap-20 gap-10 items-center justify-center lg:mt-[40px]'>
+                <div className='lg:w-[450px] lg:h-[450px] md:w-[300px] md:h-[300px] w-[160px] h-[160px] relative mb-[70px] aspect-square'>
                     <img 
                         src="https://images.cb2.com/is/image/CB2/052825_CatCarousel_Dining?&wid=800&hei=800" 
                         alt="Best Seller Item" 
@@ -42,7 +42,7 @@ function BestOffers() {
                     </h3>
                 </div>
                 
-                <div className='lg:w-[450px] lg:h-[450px]  md:w-[300px] md:h-[300px] w-[170px] h-[170px] relative mb-[70px] aspect-square'>
+                <div className='lg:w-[450px] lg:h-[450px]  md:w-[300px] md:h-[300px] w-[160px] h-[160px] relative mb-[70px] aspect-square'>
                     <img 
                         src="https://images.cb2.com/is/image/CB2/052825_CatCarousel_Dining?&wid=800&hei=800" 
                         alt="Best Seller Item" 
@@ -53,6 +53,10 @@ function BestOffers() {
                     </h3>
                 </div>
             </div>
+
+             <button className="mt-2 px-2 py-2 w-[175px] text-black border border-black/50 backdrop-blur-sm transition-all duration-300 text-lg font-medium hover:scale-105">
+                See all Offers 
+            </button>
         </div>
     )
 }
